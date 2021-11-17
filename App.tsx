1bf6f6ab60cd084b5d653bda7a706b37d687c281
Milan Milenkovic <milenkovicmilan10@gmail.com>
2021-11-17 17:24:19 +0100

    cloing part 1

--- App.tsx
+++ App.tsx
@@ -1,13 +1,11 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
 import AppContainer from './src/components/app-container'
-import MainScreen from './src/screens/main'
+import Navigator from './src/'
 
 export default function App() {
   return (
     <AppContainer>
-      <MainScreen />
+      <Navigator />
     </AppContainer>
   )
 }
